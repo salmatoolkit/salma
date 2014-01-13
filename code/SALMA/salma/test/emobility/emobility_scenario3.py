c456436773002e4f3d3a4edd0ecd8ac79888813b
kroiss <kroiss@pst.ifi.lmu.de>
2014-01-13 16:55:47 +0100

    started 3rd e-mobility scenario with real process-based handling of target positions.

--- code/SALMA/salma/test/emobility/emobility_scenario3.py
+++ code/SALMA/salma/test/emobility/emobility_scenario3.py
@@ -16,17 +16,24 @@ import pyclp
 from salma.test.emobility.emobility_test import EMobilityTest
 
 
-class EMobilityScenario2(EMobilityTest):
+def choose_target_plcs(poi):
+    x, y = mt.get_position_from_node(target_poi.id)
+    target = mt.find_closest_node(x, y, loctype="plcs")
+    r = nx.shortest_path(world_map, start.id, target)
+
+
+class EMobilityScenario3(EMobilityTest):
     NUM_OF_VEHICLES = 3
 
-    def createVehicles(self, world):
-         for i in range(EMobilityScenario2.NUM_OF_VEHICLES):
+    def create_vehicles(self, world):
+
+         for i in range(EMobilityScenario3.NUM_OF_VEHICLES):
             vehicle = Entity("v" + str(i), "vehicle")
             world.addEntity(vehicle)
 
-    def test_scenario1(self):
+    def test_scenario3(self):
         world = World.instance()
-        self.createVehicles(world)
+        self.create_vehicles(world)
 
         mgen = MapGenerator(world)
         world_map = mgen.load_from_graphml("../../../testdata/test1.graphml")
@@ -46,22 +53,15 @@ class EMobilityScenario2(EMobilityTest):
         for plcs in plcses:
             world.setConstantValue("maxCapacty", [plcs.id], 10)
 
-        targets = dict()
         for vehicle in vehicles:
             start = random.choice(starts)
             starts.remove(start)
             target_poi = random.choice(target_pois)
             target_pois.remove(target_poi)
-            x, y = mt.get_position_from_node(target_poi.id)
-            target = mt.find_closest_node(x, y, loctype="plcs")
-            targets[vehicle.id] = target
-            r = nx.shortest_path(world_map, start.id, target)
+
 
             world.setFluentValue("vehiclePosition", [vehicle.id], ("pos", start.id, start.id, 0))
-            world.setFluentValue("vehicleSpeed", [vehicle.id], 10)
-            world.setFluentValue("currentTargetPLCS", [vehicle.id], target)
             world.setFluentValue("currentTargetPOI", [vehicle.id], target_poi.id)
-            world.setFluentValue("currentRoute", [vehicle.id], r)
             world.setConstantValue("calendar", [vehicle.id], [("cal", target_poi.id, 100, 100)])
 
         uninitialized_fluent_instances, uninitialized_constant_instances = world.check_fluent_initialization()
@@ -78,7 +78,6 @@ class EMobilityScenario2(EMobilityTest):
         print("-" * 80)
         print("Uninitialized exogenous actions:")
         print(problematic_exogenous_actions)
-
         world.printState()
         self.run_until_all_targets_reached(world, world_map, 200)
 
