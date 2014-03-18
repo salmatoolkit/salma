5f9eb10d4ff1061e6a01227aeffd56bba42c2e1d
Christian Kroiss <kroiss@pst.ifi.lmu.de>
2014-03-18 01:56:39 +0100

    started to refactor seqhyptest

--- code/SALMA/salma/test/seqhyptest.py
+++ code/SALMA/salma/test/seqhyptest.py
@@ -36,7 +36,7 @@ SAMPLES = 100
 
 # probability that one experiment fails: 1 - P(all agents finish without drop) 
 # since number of steps for each agent = 10:
-P_DEFECT = 1 - ((1-P_DROP)**10)**3
+P_DEFECT = 1 - ((1 - P_DROP) ** 10) ** 3
 TOLERANCE = 0.05
 STEPS = 200
 
@@ -45,12 +45,8 @@ STEPS = 200
 ALPHA = 0.05
 BETA = 0.05
 
+
 class SequentialHypothesisTestTest(unittest.TestCase):
-
-
-    
-    
-    
     @classmethod
     def setUpClass(cls):
         domainFileName = "kroiss_aamas2014_domaindesc.ecl"
@@ -59,61 +55,42 @@ class SequentialHypothesisTestTest(unittest.TestCase):
         except SALMAException as e:
             print(e)
             raise
-        logger = logging.getLogger('agamemnon-smc')
+        logger = logging.getLogger('salma')
         logger.setLevel(logging.INFO)
         ch = logging.StreamHandler()
         logger.addHandler(ch)
-        
+
     def setUp(self):
         World.create_new_world()
         world = World.instance()
-        world.addFluent(Fluent("xpos", "integer", [("o", "object")], range=(0, 1000)))
+        world.load_declarations()
 
-        world.addFluent(Fluent("carrying", "boolean", [("r", "robot"), ("i", "item")]))
-        
-        world.addAction(DeterministicAction('move_right', [('r', 'robot')]))
-        world.addAction(DeterministicAction('grab', [('r', 'robot'), ('i', 'item')]))
-        
-        dropEvent = ExogenousAction('accidental_drop',
-                                      [
-                                       ('r', 'robot'),
-                                       ('i', 'item')
-                                      ],
-                                      BernoulliDistribution(P_DROP),
-                                      []                                     
-                                      )        
-        
-        world.addExogenousAction(dropEvent)
-        
-        
-        
+
     def __placeAgentsInColumn(self, x):
         world = World.instance()
         # y = 10
-             
+
         # : :type r: Agent 
-        for r in world.getDomain('robot'):    
+        for r in world.getDomain('robot'):
             world.setFluentValue("xpos", [r.getId()], x)
-    
-    
+
+
     def createRightMovingRobot(self, num):
         '''
         Creates a simple agent that grabs an item wit id item+num and keeps moving right as long as the agent is active 
         '''
-        
+
         mainSeq = Sequence([
-                Act("grab", [Entity.SELF, "item" + str(num)]),
-                While(EvaluationContext.PYTHON_EXPRESSION, "True",
-                   [Entity.SELF],
-                   Act("move_right", [Entity.SELF])
-                )])
-  
-      
-        
-        agent = Agent("rob" + str(num), "robot", Procedure("main",[],mainSeq))
+            Act("grab", [Entity.SELF, "item" + str(num)]),
+            While(EvaluationContext.PYTHON_EXPRESSION, "True",
+                  [Entity.SELF],
+                  Act("move_right", [Entity.SELF])
+            )])
+
+        agent = Agent("rob" + str(num), "robot", Procedure("main", [], mainSeq))
         return agent
-    
-    
+
+
     def setNoOneCarriesAnything(self):
         world = World.instance()
         robots = world.getDomain('robot')
@@ -121,29 +98,24 @@ class SequentialHypothesisTestTest(unittest.TestCase):
         for r in robots:
             for i in items:
                 world.setFluentValue('carrying', [r.id, i.id], False)
-    
-                
-        
-   
-    
-         
+
+
     @withHeader
     def testScenario(self):
-        
+
         world = World.instance()  # : :type world: World
-        
-        
-        for i in range(1,4):    
+
+        for i in range(1, 4):
             itemid = "item" + str(i)
             world.addEntity(Entity(itemid, "item"))
-           
+
             world.addAgent(self.createRightMovingRobot(i))
-            
+
         world.initialize(False)
-        for i in range(1,4):    
+        for i in range(1, 4):
             itemid = "item" + str(i)
             world.setFluentValue('xpos', [itemid], 10)
-        
+
         self.__placeAgentsInColumn(10)
         self.setNoOneCarriesAnything()
         f = '''
@@ -160,16 +132,15 @@ class SequentialHypothesisTestTest(unittest.TestCase):
         )
         '''
         world.registerProperty("f", f)
-          
-        
+
+
         #world.registerProperty("f", ("forall([r,robot], xpos(r) > 100)"))
-#         world.registerProperty("f", "forall([r,robot], xpos(r) > 50)")
-#         
+        #         world.registerProperty("f", "forall([r,robot], xpos(r) > 50)")
+        #
         #world.registerProperty("f", "xpos(rob1) > 50")
 
         print("P_DEFECT: {}".format(P_DEFECT))
-    
-       
+
         p0s = np.linspace(0.001, 0.999 - TOLERANCE, STEPS, True)
         p1s = p0s + TOLERANCE
         data = np.array([np.arange(STEPS) + 1, p0s, p1s, np.repeat(None, STEPS), np.zeros(STEPS), np.zeros(STEPS)]).T
@@ -182,14 +153,12 @@ class SequentialHypothesisTestTest(unittest.TestCase):
             spbrt = SequentialProbabilityRatioTest(row[1], row[2], ALPHA, BETA)
             accepted_hypothesis, results, trial_infos = world.run_repetitions(hypothesis_test=spbrt)
             c2 = time.clock()
-            row[3:] = [accepted_hypothesis, len(results), (c2-c1)]
-            print(row)                                 
-        
-        # header: P_DEFECT
-        np.savetxt('seqtest_aamas2014.csv', data, ["%d","%5f","%5f", "%s", "%d","%5f"], delimiter=';',
+            row[3:] = [accepted_hypothesis, len(results), (c2 - c1)]
+            print(row)
+
+            # header: P_DEFECT
+        np.savetxt('seqtest_aamas2014.csv', data, ["%d", "%5f", "%5f", "%s", "%d", "%5f"], delimiter=';',
                    header="{:5f};{:5f};{:5f};{:5f}".format(P_DEFECT, TOLERANCE, ALPHA, BETA))
-        
-        
 
 
 if __name__ == "__main__":
