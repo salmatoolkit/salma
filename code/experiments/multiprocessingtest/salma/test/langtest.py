77db53fd1519e499168ef31203d7f7ee44aae79b
Christian Kroiss <kroiss@pst.ifi.lmu.de>
2014-03-30 03:16:26 +0200

    added small test for language constructs

---
+++ code/experiments/multiprocessingtest/salma/test/langtest.py
@@ -0,0 +1,20 @@
+class Foo(object):
+    def __init__(self, factor):
+        self.__factor = factor
+    def __call__(self, a=None, b=None, *args, **kwargs):
+        print(a * self.__factor, b)
+
+    def describe(self):
+        print("Foo : %d" % self.__factor)
+
+    def __getitem__(self, item):
+        return "Item %s " % str(item)
+
+
+def bar(f: Foo=None):
+    f(4, 5)
+    f.describe()
+
+if __name__ == '__main__':
+    myf = Foo(3)
+    bar(myf)
