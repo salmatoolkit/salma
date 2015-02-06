1eab3359a33d40df57520fa2548215c236e1f2f3
Christian Kroiss <kroiss@pst.ifi.lmu.de>
2015-02-06 02:36:45 +0100

    minor test

--- code/experiments/pythonlab/matplotlibtests/pyplottest01.py
+++ code/experiments/pythonlab/matplotlibtests/pyplottest01.py
@@ -3,6 +3,7 @@ Created on 13.08.2013
 
 @author: christian
 '''
+from matplotlib.figure import Figure
 import numpy as np
 import numpy.random as rnd
 import matplotlib.pyplot as plt
@@ -11,6 +12,8 @@ mu, sigma = 100, 15
 x = mu + sigma * rnd.randn(10000)
 
 # the histogram of the data
+
+fig = plt.figure(frameon=False)
 n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
 
 
@@ -19,6 +22,10 @@ plt.ylabel('Probability')
 plt.title('Histogram of IQ')
 plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
 plt.axis([40, 160, 0, 0.03])
-plt.grid(True)
+assert isinstance(fig, Figure)
+xaxis = fig.axes[0].get_xaxis()
+print(xaxis)
 
+fig.axes[0].get_xaxis().set_visible(False)
+fig.axes[0].get_yaxis().set_visible(False)
 plt.show()
