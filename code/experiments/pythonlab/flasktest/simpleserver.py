d2ee1582847c08e4e7fa35cccba50d6f10b69d26
Christian Kroiss <kroiss@pst.ifi.lmu.de>
2015-02-12 01:20:51 +0100

    pythonlab

--- code/experiments/pythonlab/flasktest/simpleserver.py
+++ code/experiments/pythonlab/flasktest/simpleserver.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, make_response
 
 app = Flask(__name__)
 
@@ -11,7 +11,11 @@ def hello():
 
 @app.route("/hello")
 def number():
-    return "Hello World!"
+    resp = make_response("Hello Wrld!", 200)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE'
+    resp.headers['Access-Control-AllowHeaders'] = 'Content-Type'
+    return resp
 
 if __name__ == "__main__":
     app.run()
--- code/experiments/pythonlab/flasktest/static/info_retrieval.js
+++ code/experiments/pythonlab/flasktest/static/info_retrieval.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	console.log('hi');
 	$("#query_button").click(function(event) {
-		$.get('http://localhost:5000/hello', function(data) {
+		$.get('/hello', function(data) {
 			$("#response").html(data);
 		});
 		event.stopPropagation();
