d4d009b7cb83894ad6b1e7affa35ab8f7d94ce05
Christian Kroiss <christian.kroiss@gmail.com>
2015-07-06 23:22:21 +0200

    extended smctest02.py:  - SPRT driven by parameters   - lines in CSV written in file on the fly

--- code/SALMA/salma/test/smctest02.py
+++ code/SALMA/salma/test/smctest02.py
@@ -45,8 +45,10 @@ class SMCTest02(TestCase):
         self.experiment.initialize()
         # self.experiment.step_listeners.append(report_step)
 
-    @unittest.skip("too long")
     def test_confidence_interval_estimation(self):
+        if "ci" not in self.config["modes"]:
+            print("Skipping CI")
+            return
         runner = SingleProcessExperimentRunner()
         sample_length = self.config["sample_length"]
         samples = self.config["samples"]
@@ -58,22 +60,26 @@ class SMCTest02(TestCase):
         all_successes = 0
         report_lines = []
         """:type : list[dict]"""
-        for i in range(0, samples):
-            _, res, trial_infos = runner.run_trials(self.experiment,
-                                                    number_of_trials=sample_length,
-                                                    max_retrials=0)
-            print(trial_infos)
-            self.assertEqual(sample_length, len(res))
-            self.assertEqual(sample_length, len(trial_infos))
-            successes = sum(res)
-            all_successes += successes
-            ci_low, ci_up = proportion.proportion_confint(successes, len(res), alpha=alpha,
-                                                          method=method)
-            confidence_intervals.append((ci_low, ci_up))
-            report_lines.append(dict(i=i+1, successes=successes, trials=len(res)))
-
-            print("Run #{}: {} successes, CI: [{}..{}]".format(i + 1, successes, ci_low, ci_up))
-            # self.experiment.world.printState()
+        with open("smctest02_ci.csv", "w") as f:
+            f.write("I;SUCCESSES;TRIALS\n")
+            f.flush()
+            for i in range(0, samples):
+                _, res, trial_infos = runner.run_trials(self.experiment,
+                                                        number_of_trials=sample_length,
+                                                        max_retrials=0)
+                print(trial_infos)
+                self.assertEqual(sample_length, len(res))
+                self.assertEqual(sample_length, len(trial_infos))
+                successes = sum(res)
+                all_successes += successes
+                ci_low, ci_up = proportion.proportion_confint(successes, len(res), alpha=alpha,
+                                                              method=method)
+                confidence_intervals.append((ci_low, ci_up))
+                line = dict(i=i+1, successes=successes, trials=len(res))
+                f.write("{i};{successes};{trials}\n".format(**line))
+                f.flush()
+                print("Run #{}: {} successes, CI: [{}..{}]".format(i + 1, successes, ci_low, ci_up))
+                # self.experiment.world.printState()
 
         estimated_prob = all_successes / (samples * sample_length)
 
@@ -89,51 +95,53 @@ class SMCTest02(TestCase):
         print("interval hits: {} of {} = {} %".format(interval_hit, len(confidence_intervals),
                                                       interval_hit_ratio * 100.0))
 
-        with open("smctest02_ci.csv", "w") as f:
-            f.write("I;SUCCESSES;TRIALS\n")
-            for line in report_lines:
-                f.write("{i};{successes};{trials}\n".format(**line))
-
         self.assertAlmostEqual(real_prob, estimated_prob, delta=estimation_tolerance)
         self.assertTrue(interval_hit_ratio >= (1.0 - alpha))
 
-    #@unittest.skip("too long")
     def test_sprt(self):
+        if "sprt" not in self.config["modes"]:
+            print("Skipping SPRT")
+            return
         runner = SingleProcessExperimentRunner()
 
-        p = 0.1
+        p = self.config["sprt"]["p_min"]
 
         report_lines = []
         """:type : list[dict]"""
-
-        while p <= 0.1:
-            sprt = SequentialProbabilityRatioTest(p - 0.01, min(1, p + 0.01), 0.05, 0.05)
-            accepted_hypothesis, res, trial_infos = runner.run_trials(self.experiment,
-                                                                      hypothesis_test=sprt,
-                                                                      step_listeners=[report_step],
-                                                                      max_trials=200)
-            print("p={:.3}".format(p))
-            print("Accepted Hypothesis: {}\nResults: {}\nTrial Infos: {}".format(accepted_hypothesis, res, trial_infos))
-            trials = len(trial_infos)
-            print("Trials: {}".format(trials))
-            report_lines.append(dict(p=p, hyp=accepted_hypothesis, trials=trials))
-            p += 0.05
         with open("smctest02_sprt.csv", "w") as f:
             f.write("P;HYP;TRIALS\n")
-            for line in report_lines:
+            f.flush()
+            while p <= self.config["sprt"]["p_max"]:
+                sprt = SequentialProbabilityRatioTest(p - self.config["sprt"]["p_step"],
+                                                      min(1.0, p + self.config["sprt"]["p_step"]),
+                                                      self.config["sprt"]["alpha"],
+                                                      self.config["sprt"]["beta"])
+                accepted_hypothesis, res, trial_infos = runner.run_trials(self.experiment,
+                                                                          hypothesis_test=sprt,
+                                                                          max_trials=self.config["sprt"]["max_trials"])
+                print("p={:.3}".format(p))
+                print("Accepted Hypothesis: {}\nResults: {}\nTrial Infos: {}".format(accepted_hypothesis, res, trial_infos))
+                trials = len(trial_infos)
+                print("Trials: {}".format(trials))
+                line = dict(p=p, hyp=accepted_hypothesis, trials=trials)
+                p += self.config["sprt"]["p_step"]
                 f.write("{p:.3};{hyp};{trials}\n".format(**line))
+                f.flush()
 
     def calc_real_prob(self):
         fail_prob_one_robot = 0
         p_dist = 1 / (self.experiment.destination_range[1] - self.experiment.destination_range[0] + 1)
         for dist in range(self.experiment.destination_range[0], self.experiment.destination_range[1] + 1):
             for (gprobspec, dprop) in zip(self.experiment.grip_probs, self.experiment.drop_probs):
-                fail_prob_one_robot += p_dist * gprobspec[1] * geom.cdf(dist, dprop)
+                fail_prob_one_robot += gprobspec[1] * geom.cdf(dist, dprop)
+        fail_prob_one_robot *= p_dist
+        k = 7
+        print(round(fail_prob_one_robot, k))
         real_prob = (1.0 - fail_prob_one_robot) ** self.experiment.num_robots
+        print(round(real_prob, k))
+        rp2 = (1.0 - round(fail_prob_one_robot, k)) ** self.experiment.num_robots
+        print(round(rp2, k))
         return real_prob
 
     def test_calc_prob(self):
-        print("real probability: {:.50}".format(self.calc_real_prob()))
-
-    def test_visualize(self):
-        pass
+        print("real probability: {:}".format(round(self.calc_real_prob(), 6)))
