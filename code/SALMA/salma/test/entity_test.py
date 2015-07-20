4dc4e9b525edadf4dd53551e7be5e5022a69f3b3
Christian Kroiss <christian.kroiss@gmail.com>
2015-07-21 00:59:47 +0200

    added tests for comparison and sorting of entities

--- code/SALMA/salma/test/entity_test.py
+++ code/SALMA/salma/test/entity_test.py
@@ -6,16 +6,16 @@ from salma.test.world_test_base import BaseWorldTest
 
 
 class EntityTest(BaseWorldTest):
-
     def setUp(self):
         super().setUp()
         world = World.instance()
 
         self.rob1 = self.create_right_moving_mobot('rob1')
         self.rob2 = self.create_right_moving_mobot('rob2')
+        self.item3 = Entity("item3", "item")
         self.item1 = Entity("item1", "item")
         self.item2 = Entity("item2", "item")
-        world.add(self.rob1, self.rob2, self.item1, self.item2)
+        world.add(self.item3, self.rob2, self.item2, self.rob1, self.item1)
         world.initialize(False)
         self.initialize_robot("rob1", 10, 15, 1, -1, radius=5)
         self.initialize_robot("rob2", 100, 150, 0, 0, radius=8, active=False)
@@ -27,13 +27,15 @@ class EntityTest(BaseWorldTest):
         x = self.rob1.get_own_fluent_value("xpos")
         print(x)
         self.assertEqual(x, 10)
-        print("rob1: x = {}, y = {}, vx = {}, vy = {}\n".format(self.rob1.xpos, self.rob1.ypos, self.rob1.vx, self.rob1.vy))
+        print("rob1: x = {}, y = {}, vx = {}, vy = {}\n".format(self.rob1.xpos, self.rob1.ypos, self.rob1.vx,
+                                                                self.rob1.vy))
         self.assertEqual(self.rob1.xpos, 10)
         self.assertEqual(self.rob1.ypos, 15)
         self.assertEqual(self.rob1.vx, 1)
         self.assertEqual(self.rob1.vy, -1)
 
-        print("rob2: x = {}, y = {}, vx = {}, vy = {}\n".format(self.rob2.xpos, self.rob2.ypos, self.rob2.vx, self.rob2.vy))
+        print("rob2: x = {}, y = {}, vx = {}, vy = {}\n".format(self.rob2.xpos, self.rob2.ypos, self.rob2.vx,
+                                                                self.rob2.vy))
         self.assertEqual(self.rob2.xpos, 100)
         self.assertEqual(self.rob2.ypos, 150)
         self.assertEqual(self.rob2.vx, 0)
@@ -47,10 +49,10 @@ class EntityTest(BaseWorldTest):
 
         # derived fluents
         print("rob1.dist_from_origin = {}".format(self.rob1.dist_from_origin))
-        self.assertEqual(self.rob1.dist_from_origin, sqrt(self.rob1.xpos**2 + self.rob1.ypos**2))
+        self.assertEqual(self.rob1.dist_from_origin, sqrt(self.rob1.xpos ** 2 + self.rob1.ypos ** 2))
         self.assertTrue(self.rob1.moving)
         self.assertFalse(self.rob2.moving)
-        #constants
+        # constants
         self.assertEqual(self.rob1.robot_radius, 5)
         self.assertEqual(self.rob2.robot_radius, 8)
         self.assertEqual(world.gravity, 9.81)
@@ -92,9 +94,30 @@ class EntityTest(BaseWorldTest):
         world.set_gravity(4711)
         self.assertEqual(world.getConstantValue("gravity", []), 4711)
 
+    def test_ordering(self):
+        self.assertTrue(self.rob1 < self.rob2)
+        self.assertFalse(self.rob2 < self.rob1)
+        self.assertFalse(self.rob1 < self.rob1)
+        self.assertTrue(self.item1 < self.rob1)
+        self.assertTrue(self.item1 < Entity("item1b", "item"))
+        self.assertTrue(self.item1 < Entity("item1", "itemb"))
 
+        self.assertTrue(self.rob2 > self.rob1)
+        self.assertFalse(self.rob1 > self.rob2)
+        self.assertFalse(self.rob1 > self.rob1)
 
+        self.assertTrue(self.rob2 >= self.rob1)
+        self.assertFalse(self.rob1 >= self.rob2)
+        self.assertTrue(self.rob1 >= self.rob1)
 
+        self.assertTrue(self.rob1 <= self.rob2)
+        self.assertFalse(self.rob2 <= self.rob1)
+        self.assertTrue(self.rob1 <= self.rob1)
 
+        self.assertTrue(self.rob1 == self.rob1)
+        self.assertTrue(self.rob1 != self.rob2)
+        self.assertFalse(self.rob1 == self.rob2)
 
-
+        world = World.instance()
+        self.assertListEqual(sorted(world.getDomain("robot") | world.getDomain("item")),
+                             [self.item1, self.item2, self.item3, self.rob1, self.rob2])
